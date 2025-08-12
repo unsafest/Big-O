cd95b2a10b582c6baa33b559c9cabee1c9ab5a63
Dennis A. <denniszxz@hotmail.com>
2025-08-12 11:21:43 +0000

    bubble sort

--- main.js
+++ main.js
@@ -1,21 +1,61 @@
-const visualizer = document.getElementById("visualizer")
+const visualizer = document.getElementById("visualizer");
 let array = [];
 
 function generateArray(type) {
-    array = Array.from({length: 101}, (_, i) => i);
+    array = Array.from({length: 51}, (_, i) => i);
     if (type === "random") array.sort(() => Math.random() - 0.5);
     if (type === "reversed") array.reverse();
     renderBars();
 }
 
-function renderBars() {
+function renderBars(compare = [], swap = []) {
     visualizer.innerHTML = "";
-    array.forEach(value => {
+    array.forEach((value, idx) => {
         const bar = document.createElement('div');
-        bar.classList.add('bar')
+        bar.classList.add('bar');
+        if (compare.includes(idx)) bar.classList.add("comparing");
+        if (swap.includes(idx)) bar.classList.add("swapping");
         bar.style.height = `${(value / array.length) * 100}%`;
         bar.style.width = `${100 / array.length}%`;
         visualizer.appendChild(bar);
-    })
+    });
 }
 generateArray("random");
+
+function getSelectedRadioValue(name) {
+    const radios = document.getElementsByName(name);
+    for (const radio of radios) {
+        if (radio.checked) {
+            return radio.value;
+        }
+    }
+    return null;
+}
+
+function sort() {
+    const algo = getSelectedRadioValue("sort-algo")
+    if (algo === "bubble") {
+        bubbleSort(array);
+    }
+}
+
+/** ----------- Sorting Algorithms ----------- */
+
+async function bubbleSort(array) {
+    let n = array.length
+    for (let i = 0; i < n - 1; i++) {
+        let swapped = false
+        for (let j = 0; j < n - i - 1; j++) {
+            //renderBars([j, j + 1]);
+            //await new Promise(resolve => setTimeout(resolve, 40));
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                renderBars([], [j, j + 1]);
+                await new Promise(resolve => setTimeout(resolve, 40));
+                swapped = true
+            }
+        }
+        if (!swapped) break
+    }
+    renderBars();
+}
